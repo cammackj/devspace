b2f48dc97d57ebad9cf68754f13ae3f51aa074f8
Joshua Cammack <cammack110@gmail.com>
2023-06-02 03:44:20 -0600

    Update login and logout functionality

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { AccountService } from './services/account.service';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { User } from './models/user.model';
@@ -11,14 +12,26 @@ export class AppComponent implements OnInit {
     public title = 'DevSpace';
     public users: any;
 
-    constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient, private accountService: AccountService) { }
 
-    ngOnInit(): void {
+    public ngOnInit(): void {
+        this.getUsers();
+        this.setCurrentUser();
+    }
+
+    private getUsers() {
         // TODO: Move to a service
         this.http.get('https://localhost:7042/api/users').subscribe({
             next: response => this.users = response,
             error: error => console.log(error),
             complete: () => { }
         })
+    }
+
+    private setCurrentUser() {
+        const userString = localStorage.getItem('user');
+        if (!userString) return;
+        const user: User = JSON.parse(userString);
+        this.accountService.setCurrentUser(user);
     }
 }
--- client/src/app/services/account.service.ts
+++ client/src/app/services/account.service.ts
@@ -1,14 +1,37 @@
+import { User } from './../models/user.model';
 import { HttpClient } from '@angular/common/http';
 import { environment } from './../../environments/environment';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, map } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class AccountService {
+    private currentUserSource = new BehaviorSubject<User | null>(null);
+    public currentUser$ = this.currentUserSource.asObservable();
 
-constructor(private http: HttpClient) { }
-    login(model: any) {
-        return this.http.post(environment.baseUrl + 'account/login', model);
+    constructor(private http: HttpClient) {}
+
+    public login(model: any) {
+        return this.http.post<User>(environment.baseUrl + 'account/login', model).pipe(
+            map((response: User) => {
+                const user = response;
+                if (user) {
+                    localStorage.setItem('user', JSON.stringify(user));
+                    this.currentUserSource.next(user);
+                }
+            })
+        );
     }
+
+    public logout() {
+        localStorage.removeItem('user');
+        this.currentUserSource.next(null);
+    }
+
+    public setCurrentUser(user: User) {
+        this.currentUserSource.next(user);
+    }
+
 }
